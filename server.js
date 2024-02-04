436b5eb74e5a92a7859982fa5b5a80e43b98c851
Yeabsera <yeabbert@gmail.com>
2024-02-04 16:01:05 -0700

    changed to server.js

--- server.js
+++ server.js
@@ -2,18 +2,22 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.text({type: '*/*'}));
-
-
-app.post('/', function(req, res){
-    var body = req.body
-
-    if (req.get('Content-Type')){
-        console.log(req.get('Content-Type'));
-        res = res.type(req.get('Content-Type'));
+app.use(bodyParser.text({
+    type: function(req){
+        return 'text';
     }
-    res.send('hello world')
-});
+}));
+
+
+app.post('/',(req,res)=>{
+    console.log(req.body)
+    res = res.status(200)
+    var contentType = req.get('Content-Type');
+    if (contentType){
+        console.log("Content Type:" + contentType)
+    }
+    res.send(req.body)
+})
 
 app.listen(process.env.PORT || 8080, function(){
     console.log('Listening on port %d', this.address().port);
